7e644cf0db3ac68df6f09a4a45d58849704e1200
David <dbdavebrand@gmail.com>
2026-03-01 12:21:33 -0600

    refactor: rename WindMap to WeatherMap for architectural clarity

---
+++ skyapp-frontend/src/WeatherMap.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import "./WeatherMap.css";
+
+const RecenterMap = ({ lat, lon }) => {
+  const map = useMap();
+  React.useEffect(() => {
+    map.setView([lat, lon]);
+  }, [lat, lon, map]);
+  return null;
+};
+
+const WeatherMap = ({ lat, lon, theme }) => {
+  const OPENWEATHER_API_KEY = import.meta.env.VITE_WIND_MAP_KEY;
+  const isNight = theme === "night";
+
+const baseTileUrl = isNight 
+  ? "https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png" 
+  : "https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}{r}.png";; 
+
+  const cloudsUrl = `https://tile.openweathermap.org/map/clouds_new/{z}/{x}/{y}.png?appid=${OPENWEATHER_API_KEY}`;
+
+  return (
+    <div className={`weather-map-wrapper ${isNight ? "mode-night" : "mode-day"}`}>
+      <MapContainer
+        key={`${theme}-${lat}-${lon}`} 
+        center={[lat, lon]}
+        zoom={4}
+        dragging={false}
+        doubleClickZoom={false}
+        scrollWheelZoom={false}
+        boxZoom={false}
+        keyboard={false}
+        touchZoom={false}
+        zoomControl={false}
+        attributionControl={false}
+        style={{ height: "100%", width: "100%" }}      
+      >
+        {/* Layer 1: The Land */}
+        <TileLayer url={baseTileUrl} />
+        
+        {/* Layer 2: The Clouds */}
+        <TileLayer 
+          url={cloudsUrl} 
+        />
+
+        <RecenterMap lat={lat} lon={lon} />
+      </MapContainer>
+      <div className="map-overlay-scan" />
+    </div>
+  );
+};
+
+export default WeatherMap;
